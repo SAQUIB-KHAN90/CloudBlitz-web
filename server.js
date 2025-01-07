c634df575814e323d7e6360b814f8f4795b0dfcf
SAQUIB KHAN <saquibkhan890098@gmail.com>
2025-01-07 15:45:39 +0530

    added nav

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ const courseSchema = new mongoose.Schema({
   category: String,
   createdAt: { type: Date, default: Date.now },
 });
-
+    
 const Course = mongoose.model("Course", courseSchema);
 
 // Routes
@@ -35,7 +35,7 @@ app.get("/api/courses", async (req, res) => {
   } catch (err) {
     res.status(500).send(err.message);
   }
-});
+});  
 
 // 2. Add a new course
 // Add a new course
@@ -44,6 +44,40 @@ app.post("/api/courses", async (req, res) => {
     const newCourse = new Course(req.body); // Create a new course with data from the request body
     await newCourse.save(); // Save it to the database
     res.status(201).send("Course added successfully");
+  } catch (err) {
+    res.status(400).send(err.message);
+  }
+});
+
+// update for course 
+app.put("/api/courses/:id", async (req, res) => {
+  try {
+    const course = await Course.findById(req.params.id);
+    if (!course) {
+      return res.status(404).send("Course not found");
+    }
+    course.title = req.body.title;
+    course.description = req.body.description;
+    course.price = req.body.price;
+    course.instructor = req.body.instructor;
+    course.category = req.body.category;
+    await course.save();
+    res.status(200).send("Course updated successfully");
+  } catch (err) {
+    res.status(400).send(err.message);
+  }
+})
+
+
+// delete for course
+app.delete("/api/courses/:id", async (req, res) => {
+  try {
+    const course = await Course.findById(req.params.id);
+    if (!course) {
+      return res.status(404).send("Course not found");
+    }
+    await course.remove();
+    res.status(200).send("Course deleted successfully");
   } catch (err) {
     res.status(400).send(err.message);
   }
